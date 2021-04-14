f914cba679d7d70894cb541e0d600355a92e8d9f
Arthur Devresse <arthur.devresse@outlook.com>
2021-04-14 08:33:44 +0200

    edit Determinant

--- Determinant/Program.cs
+++ Determinant/Program.cs
@@ -7,6 +7,22 @@ namespace Determinant
         static void Main(string[] args)
         {
             Console.WriteLine("Determinant\n");
+
+            int[,] aMatrix = {
+                                    {2, 4},
+                                    {3, 5}
+                                };
+
+            BerekenDeterminant(aMatrix);
+
+            Console.WriteLine($"Determinant van matrix is {BerekenDeterminant(aMatrix)}");
+        }
+
+        private static object BerekenDeterminant(int[,] aMatrix)
+        {
+            throw new NotImplementedException();
         }
     }
 }
+
+
